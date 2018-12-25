87de84d761a672ae8426d6377e66c98bbe06a2c3
on-off-states <on@on-off-states.com>
2018-12-25 17:31:14 +0000

    initial commit: gatsby default starter just test editing  siteMetaData in gatsby-config.js and index.js heading and paragraph content

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Gatsby test drive</h1>
+    <p>First roll with Gatsby to see how it all hangs.</p>
+    <p>Starting from the default starter, poking around and testing out with some example content and functionality to explore possibilities and compare with Hugo.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
